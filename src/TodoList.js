0a1f8c8c2d5be554ef3209a4e15a6863b0645835
win401 <winnerv401@gmail.com>
2024-02-26 11:25:01 +0900

    Initial commit

---
+++ src/TodoList.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+const TodoList = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+
+  const addTodo = () => {
+    if (newTodo.trim() !== "") {
+      setTodos([...todos, newTodo]);
+      setNewTodo("");
+    }
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <input type="text" value={newTodo} onChange={(e) => setNewTodo(e.target.value)} />
+      <button onClick={addTodo}>Add Todo</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoList;
